d35962e7f93dfcfad6d4b6ad6f60b3cfdc41cff4
manofculturett <alfalfa06_euros@icloud.com>
2024-09-04 13:18:19 +0200

    update app

--- anamoly_app/program/anamoly.py
+++ anamoly_app/program/anamoly.py
@@ -5,6 +5,7 @@ import logging
 import json
 import numpy as np
 from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
 
 BROKER_ADDRESS='ie-databus'
 BROKER_PORT=1883
@@ -31,7 +32,8 @@ class DataAnalyzer():
         self.client.on_subscribe = self.on_subscribe
         self.client.on_message = self.on_message
         self.topic_callback = dict()
-        self.model = IsolationForest(contamination=0.03)
+        self.model_iso = IsolationForest(n_estimators=200,contamination=0.01,n_jobs=-1)
+        self.model_lof = LocalOutlierFactor(n_neighbors=2, contamination=0.01,n_jobs=-1)
 
     def on_connect(self, client, userdata, flags, rc):
         self.logger.info('Connected successfully to broker, response code {}'.format(rc))
@@ -49,7 +51,7 @@ class DataAnalyzer():
         self.logger.info('New message received on topic: {}'.format(message.topic))
         if message.topic != 'ie':
             new_msg = json.loads(message.payload)
-            self.logger.info('new message: {}'.format(new_msg))
+            # self.logger.info('new message: {}'.format(new_msg))
             try:
                 self.topic_callback[message.topic](new_msg)
             except Exception as err:
@@ -62,18 +64,37 @@ class DataAnalyzer():
         self.client.subscribe(topic)
 
     def anomaly(self, payload):
+        anomaly_entries = []  # Store all anomaly entries
         self.logger.info('Anomaly detection started')
         anomaly_state = False
+
+        # Extract data and reshape for the model
         data = [entry['_value'] for entry in payload]
-        data = np.array(data).reshape(-1,1)
-        self.model.fit(data)
-        y_pred = self.model.predict(data)
-        if -1 in y_pred:
-            self.logger.info('Anomaly detected')
-            anomaly_state = True
+        data = np.array(data).reshape(-1, 1)
+
+        # Fit the models and get predictions
+        self.model_iso.fit(data)
+        self.model_lof.fit(data)
+        anomalies_isotree = self.model_iso.predict(data)
+        anomalies_lof = self.model_lof.fit_predict(data)
+
+        self.logger.info('Anomaly results - Isolation Forest: {} | LOF: {}'.format(anomalies_isotree, anomalies_lof))
+
+        # Check for anomalies in both models
+        for i in range(len(data)):
+            if anomalies_isotree[i] == -1 and anomalies_lof[i] == -1:
+                anomaly_state = True
+                anomaly_entries.append(payload[i])  # Collect all anomalies
+
+        # Log anomaly detection result
+        if anomaly_state:
+            self.logger.info('Anomaly detected in both models')
+            self.logger.info('Anomaly entries: {}'.format(anomaly_entries))
         else:
-            self.logger.info('No anomaly detected')
-        self.client.publish('anomaly', payload=json.dumps({'anomaly':anomaly_state}))
+            self.logger.info('No anomalies detected')
+        # self.logger.info('Anomaly entries: {}'.format(anomaly_entries))
+        # Publish the anomaly detection result
+        self.client.publish('anomaly', payload=json.dumps({'anomaly': anomaly_state, 'entries': anomaly_entries}))
 
 
 
@@ -82,8 +103,8 @@ class DataAnalyzer():
             self.client.username_pw_set(USERNAME, PASSWORD)
             self.client.connect(BROKER_ADDRESS)
             self.client.loop_start()
-            self.logger.info('Subscribe to handling topic')
             self.subscribe('handling',callback=self.anomaly)
+            self.logger.info('Subscribe to handling topic')
 
         except Exception as err:
             self.logger.error('An error ocurred while handling data: {}'.format(err))
